adcadc6bee15370731c9a8e46bc345c244cf20e3
TheSingingFox <singingfox123@gmail.com>
2020-05-03 16:29:10 +0200

    Made executable for Linux systems with node installed and improved the file naming.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+#!/usr/bin/node
+
 const screenshot = require('screenshot-desktop')
 
 screenshot.listDisplays().then((displays) => {
 	console.log(displays)
 	displays.forEach(display => {
-		screenshot({ screen: display.id, filename: "/demo.jpg" + display.id})
+		screenshot({ screen: display.id, filename: "/demo" + display.id + ".jpg"})
 	})
 })
